7bc4d090bb00f9a53f6ec2c65354d806b458de8d
Marcs <mgem@itu.dk>
2019-09-22 18:07:16 +0200

    Added primative root mod n generator to sharedbase

--- diffHell.py
+++ diffHell.py
@@ -1,8 +1,12 @@
 import sympy
+from math import gcd as bltin_gcd
+import random
 class diffhellman:
     def __init__(self):
         self.sharedPrime = sympy.randprime(1,100)
-        self.sharedBase = 8
+        required_set = {num for num in range(1, self.sharedPrime) if bltin_gcd(num, self.sharedPrime) }
+        self.sharedBase = random.choice([g for g in range(1, self.sharedPrime) if required_set == {pow(g, powers, self.sharedPrime)
+                                                        for powers in range(1, self.sharedPrime)}]) % self.sharedPrime
         self.secretKey = None
         self.encodedMessage = None
 
