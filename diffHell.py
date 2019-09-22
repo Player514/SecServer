a6e06966f6d48cb3a6cb4c8424b9ac35223d5d90
Marcs <mgem@itu.dk>
2019-09-22 17:45:49 +0200

    Updated diffhellman

--- diffHell.py
+++ diffHell.py
@@ -1,25 +1,19 @@
-import random
 import sympy
 class diffhellman:
     def __init__(self):
-        self.sharedPrime = 23  #sympy.randprime(1,100)
-        self.sharedBase = 5
+        self.sharedPrime = sympy.randprime(1,100)
+        self.sharedBase = 8
         self.secretKey = None
+        self.encodedMessage = None
 
     def handshake(self, sharedPrime, sharedBase):
+        self.sharedBase = sharedBase
         self.sharedPrime = sharedPrime
-        self.sharedPrime = sharedBase
-    def encode(self, secretKey):
-        if self.secretKey != type(int):
-            print('Please type a natural number:')
-            self.secretKey = int(input())
 
+    def SendSecret(self, secret):
+        self.secretKey = secret
         return self.sharedBase ** self.secretKey % self.sharedPrime
-    def decode(self):
-        pass
 
-
-
-test = diffhellman()
-
-test.encode('dfg')
+    def decode(self, encodedMessage):
+        self.encodedMessage = encodedMessage
+        return (self.encodedMessage ** self.secretKey) % self.sharedPrime
