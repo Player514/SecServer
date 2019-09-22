36e2dc981c9864bf10210e5e295577a3df70e4d0
Player514 <andreas.klingenbrunn@gmail.com>
2019-09-22 21:15:00 +0200

    Futher Along

--- Clientfilep3.py
+++ Clientfilep3.py
@@ -1,8 +1,39 @@
 import socket
+import time
+import crypto
+
+
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 8080))
-client.send("I am CLIENT\n".encode())
-from_server = client.recv(4096)
+
+sharedPrime = 6231   # p
+sharedBase = 1236      # g
+ 
+aliceSecret = 785     # a
+NotTimeout = True
+if NotTimeout:
+	x = 0
+	client.send((str(sharedPrime) +  " " + str(sharedBase) + " " + str((sharedBase**aliceSecret) % sharedPrime)).encode())
+
+	from_server = client.recv(4096)
+	data = from_server.decode().split()
+	BobShare = int(data[0])
+	SharedSecret = (BobShare ** aliceSecret) % sharedPrime
+	print(SharedSecret)
+	mac = crypto.hmac.new(25) # default is MD5
+	client.send("I am CLIENT\n".encode())
+
+	from_server = client.recv(4096)
+	print(from_server.decode())
+	
+	client.send("I am CLIENTdoe\n".encode())
+
+	from_server = client.recv(4096)
+	print(from_server.decode())
+#	time.sleep(1)
+#	if(x > 200000000):
+#		NotTimeout = False
+
+
 client.close()
-print(from_server.decode())
--- Serverfilep3.py
+++ Serverfilep3.py
@@ -4,15 +4,41 @@ serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serv.bind(('127.0.0.1', 8080))
 serv.listen(5)
 DontClose = True
+bobSecret = 523
+B = 0
 while DontClose:
 	conn, addr = serv.accept()
 	from_client = ''
 	while True:
+
 		data = conn.recv(4096)
 		if not data: break
-		from_client += data.decode()
+		from_client = data.decode()
+		data = from_client.split()
+		sharedPrime = int(data[0])
+		sharedBase = int(data[1])
+		AliceShare = int(data[2])
+		B = (sharedBase ** bobSecret) % sharedPrime
+
+		conn.send(str(B).encode())
+		
+		SharedSecret = (AliceShare**bobSecret) % sharedPrime
+		print(SharedSecret)
+		
+		data = conn.recv(4096)
+		if not data: break
+		from_client = data.decode()
 		print(from_client)
+
 		conn.send("I am SERVER\n".encode())
+
+		data = conn.recv(4096)
+		from_client = data.decode()
+		print(from_client)
+
+		conn.send("I am SERVER\n".encode())
+
+
 	conn.close()
 	print('client disconnected')
 	DontClose = False
